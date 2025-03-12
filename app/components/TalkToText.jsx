a530e3f21a4ac5c57b58d27e8d11219a1ea0f647
Papa-Sean <smsugrue@gmail.com>
2025-03-12 14:40:08 -0400

    Enhance TalkToText component with voice selection and loading functionality

--- app/components/TalkToText.jsx
+++ app/components/TalkToText.jsx
@@ -1,9 +1,29 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const TalkToText = ({ ttsContent }) => {
 	const [isSpeaking, setIsSpeaking] = useState(false);
+	const [voices, setVoices] = useState([]);
+	const [selectedVoice, setSelectedVoice] = useState('Google UK English Female');
+
+	useEffect(() => {
+		const loadVoices = () => {
+			const voices = window.speechSynthesis.getVoices();
+			setVoices(voices);
+			const defaultVoice = voices.find(
+				(voice) => voice.name === 'Google UK English Female'
+			);
+			if (defaultVoice) {
+				setSelectedVoice(defaultVoice.name);
+			} else if (voices.length > 0) {
+				setSelectedVoice(voices[0].name);
+			}
+		};
+
+		loadVoices();
+		window.speechSynthesis.onvoiceschanged = loadVoices;
+	}, []);
 
 	const handleSpeech = () => {
 		if (isSpeaking) {
@@ -11,6 +31,10 @@ const TalkToText = ({ ttsContent }) => {
 			setIsSpeaking(false);
 		} else {
 			const utterance = new SpeechSynthesisUtterance(ttsContent);
+			const voice = voices.find((voice) => voice.name === selectedVoice);
+			if (voice) {
+				utterance.voice = voice;
+			}
 			utterance.onend = () => setIsSpeaking(false);
 			window.speechSynthesis.speak(utterance);
 			setIsSpeaking(true);
@@ -19,6 +43,7 @@ const TalkToText = ({ ttsContent }) => {
 
 	return (
 		<div className='mt-4'>
+			
 			<button
 				onClick={handleSpeech}
 				className='bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300'
